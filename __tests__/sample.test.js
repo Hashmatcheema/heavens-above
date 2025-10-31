21d486d349216d5b04f972de925f3510d9c44014
Hashmat <Hashmatcheema78952@gmail.com>
2025-10-31 22:02:37 +0500

    first commit

---
+++ __tests__/sample.test.js
@@ -0,0 +1,5 @@
+describe('Sample Test Suite', () => {
+    test('should pass basic assertion', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
---
+++ __tests__/utils.test.js
@@ -0,0 +1,15 @@
+const { getTimestamp, md5 } = require('../src/utils');
+
+describe('Utils Functions', () => {
+  test('getTimestamp converts time string to seconds', () => {
+    expect(getTimestamp('12:30:45')).toBe(12 * 3600 + 30 * 60 + 45);
+    expect(getTimestamp('00:00:00')).toBe(0);
+  });
+
+  test('md5 generates hash', () => {
+    const hash = md5('test');
+    expect(hash).toBeTruthy();
+    expect(typeof hash).toBe('string');
+    expect(hash.length).toBe(32);
+  });
+});
